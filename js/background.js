c8fea24cbda46b646633e3d666e301b68dd3b86a
kkame <kkamenet@gmail.com>
2018-01-09 20:01:57 +0900

    와이파이 동의 후 기존 페이지로 이동 처리 추가

--- js/background.js
+++ js/background.js
@@ -8,6 +8,13 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
             });
         }
 
+        else if (tab.url.indexOf('http://www.istarbucks.co.kr/util/wireless.do') !== -1) {
+
+            chrome.tabs.executeScript(tabId, {
+                file: 'js/move_origin_page.js'
+            });
+        }
+
     }
 
 
---
+++ js/move_origin_page.js
@@ -0,0 +1,2 @@
+if(window.history.length>2)
+    window.history.go(-2);
